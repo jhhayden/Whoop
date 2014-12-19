2bc2d08b46bc4d7093a49d543c5629e11b395841
John Hayden <john.hayden@here.com>
2014-12-19 09:55:41 -0500

    fizzbuzz.rb
    added main loop
    
    Signed-off-by: John Hayden <john.hayden@here.com>

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,10 @@
 #!/usr/bin/env ruby
 #
-puts "Hello John"
+# fizzbuzz will print the numbers from 1 to 100
+# if a number if a multiple of 3 it will print fizz instead
+# if a number if a multiple of 5 it will print buzz instead
+# if a number if a multiple of 15 it will print fizzbuzz instead
+#
+for i in 1..100
+       puts "i = #{i}"
+end       
