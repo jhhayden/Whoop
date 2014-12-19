b71b71694f5f3276b4e1c2c2b4c20bd5fb0bdc52
John Hayden <john.hayden@here.com>
2014-12-19 10:19:47 -0500

    added modulo logic
    testing for modulo 3, 5 or 15 to print fizz, buzz or fizzbuzz
    
    Signed-off-by: John Hayden <john.hayden@here.com>

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -6,5 +6,14 @@
 # if a number if a multiple of 15 it will print fizzbuzz instead
 #
 for i in 1..100
-       puts "i = #{i}"
+  print "i = "
+  if i % 15 == 0
+    puts "fizzbuzz"
+  elsif i % 5 == 0
+    puts "buzz"
+  elsif i % 3 == 0
+    puts "fizz"
+  else
+    puts "#{i}"
+  end
 end       
